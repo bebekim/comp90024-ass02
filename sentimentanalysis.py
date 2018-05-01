a1a3c11f7eef64781cf20d0564232edde08b0570
yhk <yh.kim@outlook.com>
2018-05-01 18:14:12 +1000

    Min/max coordinates found

--- sentimentanalysis.py
+++ sentimentanalysis.py
@@ -1,30 +1,26 @@
+import fileinput
 import json
 import pprint
 import nltk
 from nltk.stem import WordNetLemmatizer
+import matplotlib.pyplot as plt
 
-tweets = []
-geo_tweets = []
+# https://docs.python.org/3.6/library/fileinput.html
+def read_file():
+  with fileinput.input(files=('Tweets2.txt')) as f:
+    for line in f:
+      tweet = json.loads(line)
+      tweets.append(tweet)
 
-
-with open("Tweets2.txt") as f:
-  for line in f:
-    tweet = json.loads(line)
-    tweets.append(tweet)
-
-for tweet in tweets:
-  if(tweet['geo'] is not None):
-    t = {
-      "coordinates": tweet['coordinates']['coordinates'],
-      "date": tweet['created_at'],
-      "text": tweet['text']
-      }
-    geo_tweets.append(t)
-    
-
-for i in geo_tweets:
-  pprint.pprint(i)
-  
+def extract_geo():
+  for tweet in tweets:
+    if(tweet['geo'] is not None):
+      t = {
+        "coordinates": tweet['coordinates']['coordinates'],
+        "date": tweet['created_at'],
+        "text": tweet['text']
+        }
+      geo_tweets.append(t)
   
 def tokenize_text(text):
   sentences = nltk.sent_tokenize(text)
@@ -32,13 +28,18 @@ def tokenize_text(text):
   return word_tokens
 
 
-
 def remove_stopwords(tokens):
   stopword_list = nltk.corpus.stopwords.words('english')
   filtered_tokens = [token for token in tokens if token not in stopword_list]
   return filtered_tokens
 
-wordnet_lemmatizer = WordNetLemmatizer()
 
 # pick out the most common words per coordinate
 #
+if __name__ == '__main__':
+  tweets = []
+  geo_tweets = []
+  
+  
+  wordnet_lemmatizer = WordNetLemmatizer()
+
